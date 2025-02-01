caf269872993b3fd1473cbbe8b8d1c00f57ed912
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2025-02-01 18:39:13 +0100

    Update lolo.py

--- lolo.py
+++ lolo.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.title('Faire de la fummée')
-st.write('https://drive.google.com/file/d/1g-rQXl8KF-9ppIszizO8RZTYUPkG7HtA/view?usp=sharing')
+st.link_button("Télecharger & écouter", "https://drive.google.com/file/d/1g-rQXl8KF-9ppIszizO8RZTYUPkG7HtA/view?usp=sharing")
