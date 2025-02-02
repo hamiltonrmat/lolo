9ae512736b3f11e10c9fae574e4fa1bc15d677fe
Hamilton Araujo <117686961+hamiltonrmat@users.noreply.github.com>
2025-02-02 10:59:43 +0100

    Update lolo.py

--- lolo.py
+++ lolo.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.title('Faire de la fummée')
 st.link_button("Télecharger & écouter", "https://drive.google.com/file/d/1g-rQXl8KF-9ppIszizO8RZTYUPkG7HtA/view?usp=sharing")
+st.audio("lolo.mp3", format="audio/mpeg", loop=True)
 st.image("lolo.jpg")
