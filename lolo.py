71ec2107af7334b8b8c15c8b2a9cc65309172d67
hamiltonrmat <hamiltonrmat@gmail.com>
2025-02-06 13:57:45 +0100

    hhh

--- lolo.py
+++ lolo.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title('Faire de la fummée')
+st.title('Faire de la fummée !')
 st.link_button("Télecharger", "https://drive.google.com/file/d/1g-rQXl8KF-9ppIszizO8RZTYUPkG7HtA")
 st.audio("lolo.mp3", format="audio/mpeg", loop=True)
 st.image("lolo.jpg")
